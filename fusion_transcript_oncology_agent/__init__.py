a8d6f8ce6671b18756d03783cc06f9957a55a48e
abusuraihsakhri <abusuraihsakhri@gmail.com>
2026-08-26 00:00:51 +0530

    Initial Commit

---
+++ fusion_transcript_oncology_agent/__init__.py
@@ -0,0 +1,5 @@
+"""
+FusionMind: RNA-Seq & NGS Oncogenic Gene Fusion Actionability Classifier
+Classifies kinase fusions (NTRK1/2/3, RET, ALK, ROS1, FGFR2/3) and matches tumor-agnostic TRK/RET inhibitors per ESMO ESCAT scale.
+"""
+__version__ = "2.0.0-PRO"
---
+++ fusion_transcript_oncology_agent_app.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import sys
+from fusion_transcript_oncology_agent.cli import main
+
+if __name__ == '__main__':
+    sys.exit(main())
